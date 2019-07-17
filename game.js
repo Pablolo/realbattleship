48917393af0d3edc897f8e5209714394ebbbedb0
Pablolo <pablo_olondriz@hotmail.com>
2019-07-17 20:43:47 +0200

    js

---
+++ game.js
@@ -0,0 +1,14 @@
+let view = {
+	//this method takes a string message and displays it
+	// in the message display area
+	displayMessage: function(msg) {
+		let messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayMiss: function(location) {
+		//code
+	},
+	displayHit: function(location) {
+		//code
+	}
+};
